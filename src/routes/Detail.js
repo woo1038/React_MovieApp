5ac8d246eb720668d80a3fc6e189cf5b47d691db
hyukim <woo1038@hanmail.net>
2020-09-20 16:40:33 +0900

    최종 수정안

---
+++ src/routes/Detail.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Detail.css';
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if(location.state === undefined) {
+            history.push('/');
+        }
+    }
+
+    render() {
+        const { location } = this.props;
+        if(location.state) {
+            return (  
+                <div className="about__container">
+                    <span>
+                        {location.state.title}
+                    </span>
+                    <span>{location.state.summary}</span>
+                </div>
+            )      
+        } else {
+            return null;
+        }
+    }
+}
+
+export default Detail;
